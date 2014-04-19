58c37b262fb7ba51aa67c083ad477655f39ec37e
beville@gmail.com <beville@gmail.com@6c5673fe-1810-88d6-992b-cd32ca31540c>
2014-04-19 03:26:32 +0000

    Release 1.1.14-beta

---
+++ tags/1.1.14-beta/scripts/inventory.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+"""
+Print out a line-by-line list of basic tag info from all comics
+"""
+
+"""
+Copyright 2012  Anthony Beville
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import sys
+import os
+
+from comictaggerlib.comicarchive import *
+from comictaggerlib.settings import *
+from comictaggerlib.issuestring import *
+import comictaggerlib.utils
+
+def main():
+	utils.fix_output_encoding()
+	settings = ComicTaggerSettings()
+
+	style = MetaDataStyle.CIX
+
+	if len(sys.argv) < 2:
+		print >> sys.stderr, "usage:  {0} comic_folder ".format(sys.argv[0])
+		return
+	
+	filelist = utils.get_recursive_filelist( sys.argv[1:] )
+			
+	#first read in metadata from all files
+	metadata_list = []	
+	max_name_len = 2
+	for filename in filelist:
+		ca = ComicArchive(filename, settings.rar_exe_path )
+		if ca.hasMetadata( style ):
+			#make a list of paired filenames and metadata objects
+			metadata_list.append((filename, ca.readMetadata( style )))
+	
+			max_name_len = max ( max_name_len, len(filename))
+			fmt_str = u"{{0:{0}}}".format(max_name_len)
+			print >> sys.stderr, fmt_str.format( filename ) + "\r",
+			sys.stderr.flush()
+
+	print >> sys.stderr, fmt_str.format( "" ) + "\r",
+	print "-----------------------------------------------"
+	print "Found {0} comics with {1} tags".format( len(metadata_list), MetaDataStyle.name[style])
+	print "-----------------------------------------------"
+
+	# now, figure out column widths	
+	w0 = 4
+	w1 = 4
+	for filename,md in metadata_list: 
+		if not md.isEmpty:
+			w0 = max( len((os.path.split(filename)[1])), w0)
+			if md.series is not None:
+				w1 = max( len(md.series), w1)
+	w0 += 2
+	
+	# build a format string
+	fmt_str = u"{0:" + str(w0) + "} {1:" + str(w1) + "} #{2:6} ({3})"
+
+	# now sort the list by issue, and then series
+	metadata_list.sort(key=lambda x: IssueString(x[1].issue).asString(3), reverse=False)
+	metadata_list.sort(key=lambda x: unicode(x[1].series).lower()+str(x[1].year), reverse=False)
+	
+	# now print
+	for filename, md in metadata_list:
+		if not md.isEmpty:
+			print fmt_str.format(os.path.split(filename)[1]+":", md.series, md.issue, md.year), md.title
+
+if __name__ == '__main__':
+	main() 
---
+++ tags/1.1.14-beta/scripts/validate_cover.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+"""
+test archive cover against comicvine for a given issue ID
+"""
+
+"""
+Copyright 2013  Anthony Beville
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import sys
+import os
+
+import comictaggerlib.utils
+from comictaggerlib.settings import *
+from comictaggerlib.comicarchive import *
+from comictaggerlib.issueidentifier import *
+from comictaggerlib.comicvinetalker import *
+
+def main():
+
+	utils.fix_output_encoding()
+	settings = ComicTaggerSettings()
+	
+	if len(sys.argv) < 3:
+		print >> sys.stderr, "usage:  {0} comicfile issueid".format(sys.argv[0])
+		return
+	
+	filename = sys.argv[1]
+	issue_id = sys.argv[2]
+
+	if not os.path.exists(filename):
+		print >> sys.stderr, filename + ": not found!"
+		return
+		
+	ca = ComicArchive(filename, settings.rar_exe_path )
+	if not ca.seemsToBeAComicArchive():
+		print >> sys.stderr, "Sorry, but "+ filename + "  is not a comic archive!"
+		return
+
+	ii = IssueIdentifier( ca, settings )
+	
+	# calculate the hashes of the first two pages
+	cover_image_data = ca.getPage( 0 )
+	cover_hash0 = ii.calculateHash( cover_image_data )		
+	cover_image_data = ca.getPage( 1 )
+	cover_hash1 = ii.calculateHash( cover_image_data )
+	hash_list = [ cover_hash0, cover_hash1 ]
+	
+	comicVine = ComicVineTalker( )
+	result = ii.getIssueCoverMatchScore( comicVine, issue_id, hash_list, useRemoteAlternates=True, useLog=False)
+	
+	print "Best cover match score is :", result['score']
+	if result['score'] < ii.min_alternate_score_thresh:
+		print "Looks like a match!"
+	else:
+		print "Bad score, maybe not a match?"
+	print result['url']
+	
+	
+if __name__ == '__main__':
+	main() 
